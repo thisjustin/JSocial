bf75641880a432e815dd65b96308db701e84f1f4
Benjamin (Bill) Planche <benjamin.planche@aldream.net>
2013-04-23 17:34:30 +0900

    <Add> Aldream() - GitHub avatars gallery

--- jsocial.js
+++ jsocial.js
@@ -15,6 +15,7 @@ JSocial = function() {
                 Add a call to your function here (it will run after document.ready)
             */
             this.thisjustin();
+            this.Aldream();
         },
         thisjustin: function thisjustin() {
             var message,
@@ -31,6 +32,36 @@ JSocial = function() {
 
             message = '<div id="thisjustin">JSocial has been contributed to by: ' + names + '</div>';
             $('body').prepend(message);
-        }
+        },
+	Aldream: function Aldream() {
+		// Uses the users list made by thisjustin() to display a simple gallery of github avatars.
+		var style = $('<style>'
+				+ 	'#thisjustin img {'
+				+	'	filter: grayscale(1); -webkit-filter: grayscale(1); -moz-filter: grayscale(1); -o-filter: grayscale(1); -ms-filter: grayscale(1);'
+				+ 	'	transition: all 1s; -webkit-transition: all 1s; -moz-transition: all 1s; -o-transition: all 1s; -ms-transition: all 1s;'
+				+	'	border-radius: 5px;'
+				+	'}'
+				+ 	'#thisjustin img:hover {'
+				+	'	filter: grayscale(0); -webkit-filter: grayscale(0); -moz-filter: grayscale(0); -o-filter: grayscale(0); -ms-filter: grayscale(0);'
+				+	'	border-radius: 40px;'
+				+	'}'
+				+ 	'#thisjustin a { display: inline-block; margin: 1em; }'
+				+ 	'#thisjustin a > i {position: absolute; margin-left: -75px; margin-top: 80px; font-size: 1em;}'
+				+ '</style>');
+		$('html > head').append(style);
+		
+		$('#thisjustin > a').each(function(i) {
+			var a = $(this);
+			$.ajax( { // If pictures aren't displayed, you may have reached the "API Rate Limit" for your IP address.
+				url : 'https://api.github.com/users/'+ a.text(),
+				dataType : "jsonp",
+				success : function (data) {
+					a.html('<img src="'+data.data.avatar_url+'" height=80 title="'+ a.text() +'" /><i>'+ a.text() +'</i>');
+					a.css('color', 'hsl('+ (i*17) +', 50%, 50%)');
+				}
+			});
+
+		});
+	}
     };
 }();
